dd4efa3c55c597820be91669bff712adfe61a3f9
Fibrasek <gabriel1490@gmail.com>
2012-08-10 17:40:13 -0300

    Mais funções no functionsDAO

--- php/functionsDAO.php
+++ php/functionsDAO.php
@@ -18,9 +18,45 @@ class FunctionsDAO {
         start_session();
         $this->db = new Postgres();
     }
-    
-    function getUser($email = false, $name = false, $id = false){
-        
+
+    function getUser($email = false, $name = false, $id = false) {
+        if ($email) {
+            $sql = "SELECT DISTINCT * FROM users WHERE users.mail = {$mail}";
+            $user = $this->db->fetchRow($sql);
+
+            if ($user) {
+                return $user;
+            } else {
+                return null;
+            }
+        } else if ($name) {
+            $sql = "SELECT DISTINCT * FROM users WHERE users.name = {$name}";
+            $user = $this->db->fetchRow($sql);
+
+            if ($user) {
+                return $user;
+            } else {
+                return null;
+            }
+        } else if ($id) {
+            $sql = "SELECT DISTINCT * FROM users WHERE users.id = {$id}";
+            $user = $this->db->fetchRow($sql);
+
+            if ($user) {
+                return $user;
+            } else {
+                return null;
+            }
+        } else {
+            $sql = "SELECT DISTINCT * FROM users";
+            $user = $this->db->fetchAll($sql);
+
+            if ($user) {
+                return $user;
+            } else {
+                return null;
+            }
+        }
     }
 
     function authUser($user) {
@@ -41,8 +77,24 @@ class FunctionsDAO {
         }
     }
 
-    function getSiteFromUser($user) {
-        $sql = "SELECT DISTINCT * FROM site WHERE site.id_user = {$user->id}";
+    function getSiteFromUser($email = false, $name = false) {
+        $user = null;
+
+        if ($id) {
+            $sql = "SELECT DISTINCT * FROM site WHERE site.id_user = {$id}";
+
+            $site = $this->db->fetchRow($sql);
+        } else {
+            if ($email) {
+                $user = $this->getUser($email);
+            } else if ($name) {
+                $user = $this->getUser($name);
+            }
+
+            $sql = "SELECT DISTINCT * FROM site WHERE site.id_user = {$user->id}";
+
+            $site = $this->db->fetchRow($sql);
+        }
     }
 
     function getContentFromUser($user, $id = false) {
@@ -59,4 +111,5 @@ class FunctionsDAO {
     }
 
 }
+
 ?>
