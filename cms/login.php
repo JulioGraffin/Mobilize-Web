ed9392a42772e8932bf26757c3a5d8fdeedde1b7
Julio Graffin <juliograffin22@gmail.com>
2012-08-10 17:43:07 -0300

    Commit Form Login
    Commit Form Login

--- cms/login.php
+++ cms/login.php
@@ -1,17 +1,40 @@
-<html>
-
-    <head>
-            <title>CMS Mobile Web | Login no Sistema</title>
-        
-    </head>
-    <body>
-        <form>
-            <label>Login:</label>
-            <input type="text">
-            <label>Password:</label>
-            <input type="password">
-            <a href="home.php">Login</a>
-        </form>
-    </body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="pt-br">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>CMS Mobileze Web | Login no Sistema</title>
+		<meta name="author" content="CMS Mobile Web" />
+		<meta name="description" content="CMS Mobile Web - Sistema de Gerenciamento de Conteúdo" />
+		<link href="../assets/css/default.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="wrap_login">
+			<h2>
+				<strong>Log In Cms Mobilize Web</strong>
+			</h2>
+			<form name="login" id="login" action="#" method="post">
+				<div id="errors_login">
+					<span>Login ou Senha inválidos!</span>
+				</div>
+				<div class="fields">
+					<label for="login">Login:</label>
+					<input type="text" id="login" name="login">
+				</div>
+				<div class="fields">
+					<label for="password">Password:</label>
+					<input type="password" id="password" name="password">
+				</div>
+				<div class="fields">
+					<input type="submit" value="Logar">
+				</div>
+			</form>
+			<div id="footer_login">
+				<p>
+					<span>&copy; Mobilize Web | 2012</span>
+					<a href="#">suporte@radiocontrole.com.br</a>
+				</p>
+			</div>
+		</div>
+	</body>
 </html>
-   
+
