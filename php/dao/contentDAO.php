b3ff1613b587d080d4813f86496e86ffd9759355
Fibrasek <gabriel1490@gmail.com>
2012-08-10 15:13:53 -0300

    Adição de classes DAO

---
+++ php/dao/contentDAO.php
@@ -0,0 +1,47 @@
+<?php
+
+class conteudoDAO {
+
+    public $id;
+    public $title;
+    public $content;
+    public $tags;
+    public $timestamp;
+    public $id_type;
+    public $id_image;
+    public $id_site;
+
+    public function conteudoDAO($info = false) {
+        if ($info) {
+            $this->init($info);
+        }
+    }
+
+    private function init($info) {
+        foreach ($info as $property => $value) {
+            if (property_exists(__CLASS__, $property)) {
+                $this->$property = $value;
+            } else {
+                $errors[] = "Atributo {$property} inexistente";
+            }
+        }
+        if (count(@$errors) > 0) {
+            print("OCORREU UM ERRO COM OS ATRIBUTOS");
+            print_r($errors);
+            die();
+        }
+        return $this;
+    }
+
+    public function validateFields() {
+        foreach ($this as $property => $value) {
+            if (empty($value) && $property != "pk_owner") {
+                addError("Existem campos vázios! $property");
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+?>
