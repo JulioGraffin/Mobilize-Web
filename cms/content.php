1940b13cd3c3c13f31b778353135e4f6f4268a96
Julio Graffin <juliograffin22@gmail.com>
2012-08-13 16:11:02 -0300

    content.php
    content.php

--- cms/content.php
+++ cms/content.php
@@ -7,6 +7,10 @@
 		<meta name="author" content="CMS Mobileze Web" />
 		<meta name="description" content="CMS Mobileze Web - Sistema de Gerenciamento de Conteúdo" />
 		<link href="../assets/css/default.css" type="text/css" rel="stylesheet">
+		<link href="../assets/css/fancydropdown.css" type="text/css" rel="stylesheet">
+		
+		<script type="text/javascript" src="../assets/js/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="../assets/js/fancydropdown.js"></script>
 	</head>
 	<body>
 		<div id="bar">
@@ -15,12 +19,52 @@
 					<img src="../assets/images/logo/logo_radiocontrolesms.png" alt="" title="">
 				</a>
 			</h1>
+			<div id="nav">
+				<ul class="tabs">
+					<li class="hasmore"><a id="btn_content" href="#"><span>Conteúdo</span></a>
+						<ul class="dropdown">
+							<li><a href="#">Menu item 1</a></li>
+							<li><a href="#">Menu item 2</a></li>
+							<li><a href="#">Menu item 3</a></li>
+							<li class="last"><a href="#">Menu item 6</a></li>
+						</ul>
+					</li>
+					<li class="hasmore"><a id="btn_promotions" href="#"><span>Promoções</span></a>
+						<ul class="dropdown">
+							<li><a href="#">Topic 1</a></li>
+							<li><a href="#">Topic 2</a></li>
+							<li><a href="#">Topic 3</a></li>
+							<li class="last"><a href="#">Topic 4</a></li>
+						</ul>
+					</li>
+					<li class="hasmore"><a id="btn_custom" href="/about/#networks"><span>Customização</span></a>
+						<ul class="dropdown">
+							<li><a href="#">Twitter</a></li>
+							<li><a href="#">posterous</a></li>
+							<li><a href="#">SpeakerSite</a></li>
+							<li><a href="#">LinkedIn</a></li>
+							<li class="last"><a href="#">See more&hellip;</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<h2>
+				<strong>Julio Graffin</strong>
+				<a href="#">Sair</a>
+			</h2>
 		</div>
 		<div id="container">
-			
+			<div id="content">
+				
+			</div>
 		</div>
 		<div id="footer">
-			
+			<div id="content_footer">
+				<p>
+					<span>&copy; Rádio Controle SMS | 2012</span>
+					<a href="mailto:suporte@radiocontrolesms.com.br">suporte@radiocontrolesms.com.br</a>
+				</p>
+			</div>
 		</div>		
 	</body>
 </html>
